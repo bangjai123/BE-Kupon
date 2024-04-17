9b2f6c8d8e8933f04682eceecbd9e911f4169a29
bangjai123 <znaufalilmi@gmail.com>
2024-04-17 19:48:58 +0700

    CI/CD pertama

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+
 plugins {
     java
-    id("org.springframework.boot") version "3.2.4"
+    jacoco
+    id("org.sonarqube") version "4.4.1.3373"
+    id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
 }
 
@@ -21,15 +24,50 @@ repositories {
     mavenCentral()
 }
 
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+    testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
+tasks.register<Test>("unitTest"){
+    description = "Run unit tests."
+    group = "verification"
 
-tasks.withType<Test> {
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+}
+tasks.register<Test>("functionalTest"){
+    description = "Runs functional tests."
+    group = "verification"
+
+    filter {
+        includeTestsMatching("*FunctionalTest")
+    }
+}
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+}
+tasks.test{
+    filter{
+        excludeTestsMatching("*FunctionalTest")
+    }
+    finalizedBy(tasks.jacocoTestReport)
+}
+tasks.jacocoTestReport{
+    dependsOn(tasks.test)
 }
