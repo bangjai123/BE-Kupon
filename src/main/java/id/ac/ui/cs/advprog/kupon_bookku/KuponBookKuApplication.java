c5f7773c8d39a79071182b03a242fa383cbd0d18
bangjai123 <znaufalilmi@gmail.com>
2024-04-17 19:33:39 +0700

    inisiliasi

---
+++ src/main/java/id/ac/ui/cs/advprog/kupon_bookku/KuponBookKuApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.kupon_bookku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuponBookKuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuponBookKuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/kupon_bookku/KuponBookKuApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.kupon_bookku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuponBookKuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
