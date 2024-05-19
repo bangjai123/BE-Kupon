a08a4f19bdff4e1f991904b85f97e805a1351919
bangjai123 <znaufalilmi@gmail.com>
2024-05-19 20:41:37 +0700

    lesgo

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@
 plugins {
     java
     jacoco
-    id("org.sonarqube") version "4.4.1.3373"
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
 }
