5a6bfc077fd06718570196a99856f701b24bb587
bangjai123 <znaufalilmi@gmail.com>
2024-05-20 01:12:48 +0700

    coba lagi

--- src/main/java/id/ac/ui/cs/advprog/kupon_bookku/KuponBookKuApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/kupon_bookku/KuponBookKuApplication.java
@@ -3,9 +3,10 @@ package id.ac.ui.cs.advprog.kupon_bookku;
 import id.ac.ui.cs.advprog.kupon_bookku.model.Kupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableAsync
 public class KuponBookKuApplication {
 
