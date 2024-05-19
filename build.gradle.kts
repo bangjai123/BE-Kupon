86cd250cc44b00a2bce9cdc0c61f55519c06e28e
bangjai123 <znaufalilmi@gmail.com>
2024-05-19 19:51:29 +0700

    mencoba deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,41 +47,41 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 
 }
-//tasks.register<Test>("unitTest"){
-//    description = "Run unit tests."
-//    group = "verification"
-//
-//    filter {
-//        excludeTestsMatching("*FunctionalTest")
-//    }
-//}
-//tasks.register<Test>("functionalTest"){
-//    description = "Runs functional tests."
-//    group = "verification"
-//
-//    filter {
-//        includeTestsMatching("*FunctionalTest")
-//    }
-//}
-//
-//tasks.withType<Test>().configureEach {
-//    useJUnitPlatform()
-//}
-//
-//tasks.test {
-//    filter {
-//        excludeTestsMatching("*FunctionalTest")
-//    }
-//
-//    finalizedBy(tasks.jacocoTestReport)
-//}
-//
-//tasks.jacocoTestReport {
-//    dependsOn(tasks.test)
-//    reports {
-//        xml.required.set(true)
-//        csv.required.set(true)
-//        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
-//    }
-//}
+tasks.register<Test>("unitTest"){
+    description = "Run unit tests."
+    group = "verification"
+
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+}
+tasks.register<Test>("functionalTest"){
+    description = "Runs functional tests."
+    group = "verification"
+
+    filter {
+        includeTestsMatching("*FunctionalTest")
+    }
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
+tasks.test {
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
 
--- src/test/java/id/ac/ui/cs/advprog/kupon_bookku/service/KuponServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/kupon_bookku/service/KuponServiceTest.java
@@ -1,7 +1,5 @@
+
 package id.ac.ui.cs.advprog.kupon_bookku.service;
 
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 public class KuponServiceTest {
 }
