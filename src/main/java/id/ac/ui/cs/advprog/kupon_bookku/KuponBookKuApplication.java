d598910e9921359c41849665f5993715c2d51d11
bangjai123 <znaufalilmi@gmail.com>
2024-05-20 01:28:30 +0700

    lagi

--- src/main/java/id/ac/ui/cs/advprog/kupon_bookku/KuponBookKuApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/kupon_bookku/KuponBookKuApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 @EnableAsync
 public class KuponBookKuApplication {
 
